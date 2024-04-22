4e44cb8f8d4c0777d63b860ddcbf00e54276a6d6
Toma Bogdan <bogdantoma2000@gmail.com>
2024-04-22 16:10:16 +0300

    fix - children don't render

--- ReactOnlineShop/src/App.jsx
+++ ReactOnlineShop/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import getItems from "./modules/fakeStoreApi";
 import "./styles/App.css";
 import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [productList, setData] = useState(null);
@@ -22,6 +23,7 @@ function App() {
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 }
