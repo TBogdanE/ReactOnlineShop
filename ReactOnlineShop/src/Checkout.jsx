b72e6e68c6a56c1dffe8f2789ce8d4b4b4cd2f46
Toma Bogdan <bogdantoma2000@gmail.com>
2024-04-26 16:20:04 +0300

    add - display 'empty cart' when no products in the cart

--- ReactOnlineShop/src/Checkout.jsx
+++ ReactOnlineShop/src/Checkout.jsx
@@ -1,9 +1,30 @@
-import Header from "./components/Header";
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import CreateCardElement from "./components/CartElement";
+import "./styles/Checkout.css";
 
 export default function Checkout() {
+  const [list, updateList] = useState([]);
+  const [productList, shoppingCart, updateCart] = useOutletContext();
+
+  useEffect(() => {
+    updateList(shoppingCart);
+  }, [shoppingCart]);
+
   return (
     <>
-      <div id="checkout-section">Hello</div>
+      <div id="checkout-view">
+        {list.length === 0 && <div id="empty-cart">Your cart is empty!</div>}
+        {list.map((item) => {
+          return (
+            <CreateCardElement
+              item={item}
+              cart={shoppingCart}
+              updateCart={updateCart}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
